3c27e1720ba73781a049365cb56b6d0ccfc3ed96
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2017-11-29 16:09:54 -0600

    adds basic code to dashboard.jsx, and changes content>home template to use dashboard.jsx, instead of hello_react

---
+++ app/javascript/packs/dashboard.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Dashboard extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    React Fit
+                </h1>
+                <p>Your personal fitness dashboard:</p>
+            </div>
+        )
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+        <Dashboard  />,
+        document.body.appendChild(document.createElement('div')),
+    )
+})
