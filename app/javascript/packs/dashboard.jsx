feba11ae98f580f2144a6ca3555a159d2396ac5d
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2017-11-29 18:07:11 -0600

    add fitbit log in to dashboard

--- app/javascript/packs/dashboard.jsx
+++ app/javascript/packs/dashboard.jsx
@@ -10,12 +10,21 @@ class Dashboard extends React.Component {
 
 
     render() {
+        //  <!-- need to change the following in URL below: client_id, redirect_uri -->
         return (
-            <div className="container">
-                <header className="text-center">
-                    <h1 className="page-header">React Fit</h1>
-                    <p className="lead">Your personal fitness dashboard:</p>
-                </header>
+            <div>
+                <div className="container">
+                    <header className="text-center">
+                        <h1 className="page-header">React Fit</h1>
+                        <p className="lead">Your personal fitness dashboard:</p>
+                    </header>
+                </div>
+
+                <div className="row text-center">
+                    <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=228HFZ&redirect_uri=http%3A%2F%localhost%3A%2Ffitbit_auth&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800\n" >
+                        Log in with fitbit
+                    </a>
+                </div>
             </div>
         )
     }
