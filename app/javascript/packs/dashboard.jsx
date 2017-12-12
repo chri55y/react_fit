0c2923024b1ea39558b16f4848656c18725378ab
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2017-12-11 19:18:27 -0600

    adds distance to lifetime stats panes, above steps

--- app/javascript/packs/dashboard.jsx
+++ app/javascript/packs/dashboard.jsx
@@ -93,6 +93,13 @@ class Dashboard extends React.Component {
                         <div className="panel panel-default">
                             <div className="panel-heading"><h4>Lifetime Stats</h4></div>
                             <div className="panel-body">
+                                <h4>Distance:</h4>
+                                <p>Total: {this.state.lifetimeTotals.distance}</p>
+                                {this.state.loggedIn &&
+                                <p>
+                                    Best: {this.state.lifetimeBest.distance.value} on {this.state.lifetimeBest.distance.date}
+                                </p>
+                                }
                                 <h4>Steps:</h4>
                                 <p>Total: {this.state.lifetimeTotals.steps}</p>
                                 {this.state.loggedIn &&
