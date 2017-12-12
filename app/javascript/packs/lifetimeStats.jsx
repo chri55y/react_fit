9dfca9b5e23493d20fcb13400d004e7189bac14d
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2017-12-11 19:28:35 -0600

    define and export const component LifetimeStats

--- app/javascript/packs/lifetimeStats.jsx
+++ app/javascript/packs/lifetimeStats.jsx
@@ -1,21 +1,25 @@
+import React from 'react';
 
+const LifetimeStats = ({}) =>
 
-<div className="panel panel-default">
-    <div className="panel-heading"><h4>Lifetime Stats</h4></div>
-    <div className="panel-body">
-        <h4>Distance:</h4>
-        <p>Total: {this.state.lifetimeTotals.distance}</p>
-        {this.state.loggedIn &&
-        <p>
-            Best: {this.state.lifetimeBest.distance.value} on {this.state.lifetimeBest.distance.date}
-        </p>
-        }
-        <h4>Steps:</h4>
-        <p>Total: {this.state.lifetimeTotals.steps}</p>
-        {this.state.loggedIn &&
-        <p>
-            Best: {this.state.lifetimeBest.steps.value} on {this.state.lifetimeBest.steps.date}
-        </p>
-        }
+    <div className="panel panel-default">
+        <div className="panel-heading"><h4>Lifetime Stats</h4></div>
+        <div className="panel-body">
+            <h4>Distance:</h4>
+            <p>Total: {this.state.lifetimeTotals.distance}</p>
+            {this.state.loggedIn &&
+            <p>
+                Best: {this.state.lifetimeBest.distance.value} on {this.state.lifetimeBest.distance.date}
+            </p>
+            }
+            <h4>Steps:</h4>
+            <p>Total: {this.state.lifetimeTotals.steps}</p>
+            {this.state.loggedIn &&
+            <p>
+                Best: {this.state.lifetimeBest.steps.value} on {this.state.lifetimeBest.steps.date}
+            </p>
+            }
+        </div>
     </div>
-</div>
+
+export default LifetimeStats
